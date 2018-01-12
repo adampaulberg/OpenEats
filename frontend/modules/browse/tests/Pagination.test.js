089e79d7eedb68628048481bbdbe1027aa550ca3
ryan.noelk <ryannoelk@gmail.com>
2018-01-12 17:15:05 -0500

    up

--- frontend/modules/browse/tests/Pagination.test.js
+++ frontend/modules/browse/tests/Pagination.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Loading from '../components/Pagination';
-import renderer from 'react-test-renderer';
+import Pagination from '../components/Pagination';
+import createComponentWithRouter from '../../../jest_mocks/createComponentWithRouter';
 
 test('Loading component test', () => {
-  const component = renderer.create(
-    <Pagination/>
+  const buildUrl = jest.fn();
+  const component = createComponentWithRouter(
+    <Pagination offset={0} limi={6} coun={3} buildUrl={ buildUrl }/>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
